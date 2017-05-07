6e7c326e074a795417c7422bc6ac034b57dce480
ewingtsui <ewing.tsui@gmail.com>
2017-05-07 12:07:05 +0800

    7.1 建立一個 EFController 並練習完整的 Entity Framework 資料存取方法 (CRUD)

---
+++ MVC5Course/Controllers/EFController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC5Course.Models;
+
+namespace MVC5Course.Controllers
+{
+    public class EFController : Controller
+    {
+        FabricsEntities db = new FabricsEntities();
+
+        // GET: EF
+        public ActionResult Index()
+        {
+            var all = db.Product.AsQueryable();
+
+            var data = all.Where(p => p.Active == true && p.ProductName.Contains("Black"))
+                .OrderByDescending(p => p.ProductId);
+
+            ////下面的型別都不同
+            //var data1 = all.Where(p => p.ProductId == 1);
+            //var data2 = all.FirstOrDefault(p => p.ProductId == 1);
+            //var data3 = db.Product.Find(1);
+
+
+            return View(data);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Product.Add(product);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var item = db.Product.Find(id);
+            return View(item);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                var item = db.Product.Find(id);
+                item.ProductName = product.ProductName;
+                item.Price = product.Price;
+                item.Stock = product.Stock;
+                item.Active = product.Active;
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            var item = db.Product.Find(id);
+            return View(item);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult Delete(int id)
+        {
+            var product = db.Product.Find(id);
+            db.Product.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
