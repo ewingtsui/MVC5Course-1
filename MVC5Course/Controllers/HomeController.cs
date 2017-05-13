2918f9757c9d1208e48d610dbbcbbe40426e6bb5
ewingtsui <ewing.tsui@gmail.com>
2017-05-13 14:39:27 +0800

    用 PartialView 來回應特定成功轉向的網頁內容

--- MVC5Course/Controllers/HomeController.cs
+++ MVC5Course/Controllers/HomeController.cs
@@ -26,10 +26,30 @@ namespace MVC5Course.Controllers
 
             return View();
         }
-        
+
         public ActionResult Test()
         {
             return View();
+        }
+
+        //建立PartilaView, 從About頁面顯示出來
+        public ActionResult PartialAbout()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView("About");
+            }
+            else
+            {
+                return View("About");
+            }
+        }
+
+        public ActionResult SomeAction()
+        {
+            return PartialView("SuccessRedirect", "/");
         }
     }
 }
